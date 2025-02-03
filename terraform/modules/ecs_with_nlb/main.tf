ce4457b52ac3baebf16e1650340a32e6fb0b17e9
Lidor Ettinger <lidor.ettinger@gmail.com>
2025-02-02 21:35:48 -0500

    increase desire count

--- terraform/modules/ecs_with_nlb/main.tf
+++ terraform/modules/ecs_with_nlb/main.tf
@@ -97,7 +97,7 @@ resource "aws_ecs_service" "ecs_service" {
   name            = "${var.name}-service"
   cluster         = aws_ecs_cluster.ecs_cluster.id
   task_definition = aws_ecs_task_definition.ecs_task.arn
-  desired_count   = 1
+  desired_count   = 2
   launch_type     = "FARGATE"
 
   network_configuration {
