d8be9d6c641c0e864f210d10aa3cd2209cd453ad
Lidor Ettinger <lidor.ettinger@gmail.com>
2025-02-05 08:42:45 -0500

    attach vpc to subnets

--- terraform/services/squid/data.tf
+++ terraform/services/squid/data.tf
@@ -11,8 +11,8 @@ data "aws_vpc" "selected" {
 
 data "aws_subnets" "public" {
   filter {
-    name   = "tag:tier"
-    values = [var.env]
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
   }
 
   tags = {
